0fad7aaa5830d6cf47b5f95b7ca05332483a83b4
kristinaordenans <okristin92@gmail.com>
2024-04-30 11:51:20 +0300

    test four

--- js/swiper.js
+++ js/swiper.js
@@ -1,8 +1,28 @@
 // core version + navigation, pagination modules:
-import Swiper from 'swiper';
+// import Swiper from 'swiper';
+import Swiper from "https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js";
 // import { Navigation, Pagination } from 'swiper/modules';
 // import Swiper and modules styles
-import 'swiper/css';
-import 'swiper/css/navigation';
-import 'swiper/css/pagination';
+// import 'swiper.css';
+// import 'swiper/css/navigation';
+// import 'swiper/css/pagination';
 
+
+document.addEventListener('DOMContentLoaded', function() {
+    const swiper = new Swiper('.swiper', {
+      loop: true,
+      centeredSlides: true,
+      
+      slidesPerView: 3,
+      pagination: {
+          el: ".swiper-pagination",
+          clickable: true,
+          speed: 1000
+      },
+      speed: 1000,
+      navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+      }
+                });
+    });
