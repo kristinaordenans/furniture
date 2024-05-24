47af111824e0a9a7ed1a520c95f90db3c5f37fab
kristinaordenans <okristin92@gmail.com>
2024-05-24 10:15:25 +0300

    Initial commit

--- js/form.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    const form = document.getElementById('form-modal');
-    form.addEventListener('submit', formSend);
-
-    function formSend(e) {
-        e.preventDefault();
-    }
-
-})
