b7d38defd1f4415416488dc2c11c8d7838db5a41
kristinaordenans <okristin92@gmail.com>
2024-05-31 14:47:44 +0300

    add scroll to top btn

---
+++ js/scrollToTop.js
@@ -0,0 +1,18 @@
+let mybutton = document.getElementById("scrollToTopBtn");
+
+// Коли користувач прокручує вниз 20px від верхньої частини документа, покажіть кнопку
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 40 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    } else {
+        mybutton.style.display = "none";
+    }
+}
+
+// Коли користувач натискає на кнопку, прокрутіть вгору до вершини документа
+mybutton.addEventListener('click', function() {
+    window.scrollTo({ top: 0, behavior: 'smooth' });
+    console.log('ok')
+});
