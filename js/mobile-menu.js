a48bef675e7249615a3a7587f0a4aebbcfb7845a
kristinaordenans <okristin92@gmail.com>
2024-06-19 09:54:54 +0300

    add adapt 768 for all pages

---
+++ js/mobile-menu.js
@@ -0,0 +1,23 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const burger = document.querySelector('.burger');
+    const nav = document.querySelector('.nav_list');
+    const navLinks = document.querySelectorAll('.nav-list-item');
+
+    burger.addEventListener('click', () => {
+        // Toggle Nav
+        nav.classList.toggle('nav-active');
+
+        // Animate Links
+        navLinks.forEach((link, index) => {
+            if (link.style.animation) {
+                link.style.animation = '';
+            } else {
+                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
+            }
+        });
+
+        // Burger Animation
+        burger.classList.toggle('toggle');
+    });
+});
+
