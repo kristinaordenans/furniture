dd28d4fc9d76c56de9eb51c7c9f46a4f95ccb4d2
kristinaordenans <okristin92@gmail.com>
2024-06-20 08:59:34 +0300

    add burger menu to main page

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,23 +1,17 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const burger = document.querySelector('.burger');
-    const nav = document.querySelector('.nav_list');
-    const navLinks = document.querySelectorAll('.nav-list-item');
+    const burger = document.getElementById('burger');
+    const navList = document.getElementById('navList');
+    const navLinks = document.querySelectorAll('.nav-link');
 
     burger.addEventListener('click', () => {
-        // Toggle Nav
-        nav.classList.toggle('nav-active');
-
-        // Animate Links
-        navLinks.forEach((link, index) => {
-            if (link.style.animation) {
-                link.style.animation = '';
-            } else {
-                link.style.animation = `navLinkFade 0.5s ease forwards ${index / 7 + 0.3}s`;
-            }
-        });
-
-        // Burger Animation
+        navList.classList.toggle('active');
         burger.classList.toggle('toggle');
     });
-});
 
+    navLinks.forEach(link => {
+        link.addEventListener('click', () => {
+            navList.classList.remove('active');
+            burger.classList.remove('toggle');
+        });
+    });
+});
