d62048a5405ffe811d22222708478b98f8e8d0b6
kristinaordenans <okristin92@gmail.com>
2024-07-04 16:19:34 +0300

    fix scale script

--- js/scale.js
+++ js/scale.js
@@ -2,15 +2,20 @@ function setViewport() {
     // Отримуємо поточний метатег viewport
     var viewportMeta = document.getElementById('viewportMeta');
 
-    // Визначаємо роздільну здатність пристрою
-    var devicePixelRatio = window.devicePixelRatio || 1;
-    var screenResolution = Math.sqrt(screen.width * screen.width + screen.height * screen.height) / (screen.width / devicePixelRatio);
+    // Перевіряємо, чи знайдений метатег viewport
+    if (viewportMeta) {
+        // Визначаємо роздільну здатність пристрою
+        var devicePixelRatio = window.devicePixelRatio || 1;
+        var screenResolution = Math.sqrt(screen.width * screen.width + screen.height * screen.height) / (screen.width / devicePixelRatio);
 
-    // Змінюємо значення initial-scale залежно від роздільної здатності
-    if (devicePixelRatio > 1.5 || screenResolution >= 144) {
-        viewportMeta.setAttribute('content', 'width=device-width, initial-scale=0.75');
+        // Змінюємо значення initial-scale залежно від роздільної здатності
+        if (devicePixelRatio > 1.5 || screenResolution >= 144) {
+            viewportMeta.setAttribute('content', 'width=device-width, initial-scale=0.75');
+        } else {
+            viewportMeta.setAttribute('content', 'width=device-width, initial-scale=1.0');
+        }
     } else {
-        viewportMeta.setAttribute('content', 'width=device-width, initial-scale=1.0');
+        console.error('Viewport meta tag not found');
     }
 }
 
