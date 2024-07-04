7525a5b867c5794af3ca1add840693f1137dd85b
kristinaordenans <okristin92@gmail.com>
2024-07-04 22:45:16 +0300

    fix id in metatag head for js scale

--- js/scale.js
+++ js/scale.js
@@ -1,23 +1,25 @@
-function setViewport() {
-    // Отримуємо поточний метатег viewport
-    var viewportMeta = document.getElementById('viewportMeta');
+document.addEventListener('DOMContentLoaded', function() {
+    function setViewport() {
+        // Отримуємо поточний метатег viewport
+        var viewportMeta = document.getElementById('viewportMeta');
 
-    // Перевіряємо, чи знайдений метатег viewport
-    if (viewportMeta) {
-        // Визначаємо роздільну здатність пристрою
-        var devicePixelRatio = window.devicePixelRatio || 1;
-        var screenResolution = Math.sqrt(screen.width * screen.width + screen.height * screen.height) / (screen.width / devicePixelRatio);
+        // Перевіряємо, чи знайдений метатег viewport
+        if (viewportMeta) {
+            // Визначаємо роздільну здатність пристрою
+            var devicePixelRatio = window.devicePixelRatio || 1;
+            var screenResolution = Math.sqrt(screen.width * screen.width + screen.height * screen.height) / (screen.width / devicePixelRatio);
 
-        // Змінюємо значення initial-scale залежно від роздільної здатності
-        if (devicePixelRatio > 1.5 || screenResolution >= 144) {
-            viewportMeta.setAttribute('content', 'width=device-width, initial-scale=0.75');
+            // Змінюємо значення initial-scale залежно від роздільної здатності
+            if (devicePixelRatio > 1.5 || screenResolution >= 144) {
+                viewportMeta.setAttribute('content', 'width=device-width, initial-scale=0.75');
+            } else {
+                viewportMeta.setAttribute('content', 'width=device-width, initial-scale=1.0');
+            }
         } else {
-            viewportMeta.setAttribute('content', 'width=device-width, initial-scale=1.0');
+            console.error('Viewport meta tag not found');
         }
-    } else {
-        console.error('Viewport meta tag not found');
     }
-}
 
-// Викликаємо функцію при завантаженні сторінки
-window.addEventListener('load', setViewport);
+    // Викликаємо функцію при завантаженні сторінки
+    setViewport();
+});
