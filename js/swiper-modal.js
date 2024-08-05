7ced0468a8b5f8db917c17f7c136c4b6df16c1ab
kristinaordenans <okristin92@gmail.com>
2024-08-05 11:17:52 +0300

    fix swiper for desctop

--- js/swiper-modal.js
+++ js/swiper-modal.js
@@ -1,12 +1,22 @@
 import Swiper from "https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js";
 
-document.addEventListener('DOMContentLoaded', function() {
-    window.swiper = new Swiper('.swiper', {
-        slidesPerView: 1,
-        spaceBetween: 30,
-        navigation: {
-            nextEl: '.swiper-button-next',
-            prevEl: '.swiper-button-prev',
-        },
+newFunction();
+
+function newFunction() {
+    document.addEventListener('DOMContentLoaded', function () {
+        window.swiper = new Swiper('.swiper', {
+            slidesPerView: 1,
+            spaceBetween: 30,
+            navigation: {
+                nextEl: '.swiper-button-next',
+                prevEl: '.swiper-button-prev',
+            },
+            // breakpoints: {
+            //     768: {
+            //         slidesPerView: 3,
+            //         spaceBetween: -10,
+            //     },
+            // },
+        });
     });
-})
+}
