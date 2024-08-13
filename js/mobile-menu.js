f00a6fe5b7aad860bcad6f9fb89fd581af2f5591
kristinaordenans <okristin92@gmail.com>
2024-08-13 08:13:12 +0300

    back to last option

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,48 +1,48 @@
-// document.addEventListener('DOMContentLoaded', () => {
-//     const burger = document.getElementById('burger');
-//     const navList = document.getElementById('navList');
-//     const navLinks = document.querySelectorAll('.nav-link');
-
-//     burger.addEventListener('click', () => {
-//         navList.classList.toggle('active');
-//         burger.classList.toggle('toggle');
-//         document.body.classList.add('lock-scroll');
-//     });
-
-//     navLinks.forEach(link => {
-//         link.addEventListener('click', () => {
-//             navList.classList.remove('active');
-//             burger.classList.remove('toggle');
-//             document.body.classList.remove('lock-scroll');
-
-//         });
-//     });
-// });
-
-
 document.addEventListener('DOMContentLoaded', () => {
     const burger = document.getElementById('burger');
     const navList = document.getElementById('navList');
+    const navLinks = document.querySelectorAll('.nav-link');
 
     burger.addEventListener('click', () => {
-        if (!navList.hasAttribute('data-initialized')) {
-            // Ініціалізуємо меню лише при першому кліку
-            navList.classList.add('active');
-            navList.setAttribute('data-initialized', 'true');
-        } else {
-            navList.classList.toggle('active');
-        }
+        navList.classList.toggle('active');
         burger.classList.toggle('toggle');
-        document.body.classList.toggle('lock-scroll');
+        document.body.classList.add('lock-scroll');
     });
-
-    const navLinks = navList.querySelectorAll('.nav-link');
 
     navLinks.forEach(link => {
         link.addEventListener('click', () => {
             navList.classList.remove('active');
             burger.classList.remove('toggle');
             document.body.classList.remove('lock-scroll');
+
         });
     });
 });
+
+
+// document.addEventListener('DOMContentLoaded', () => {
+//     const burger = document.getElementById('burger');
+//     const navList = document.getElementById('navList');
+
+//     burger.addEventListener('click', () => {
+//         if (!navList.hasAttribute('data-initialized')) {
+//             // Ініціалізуємо меню лише при першому кліку
+//             navList.classList.add('active');
+//             navList.setAttribute('data-initialized', 'true');
+//         } else {
+//             navList.classList.toggle('active');
+//         }
+//         burger.classList.toggle('toggle');
+//         document.body.classList.toggle('lock-scroll');
+//     });
+
+//     const navLinks = navList.querySelectorAll('.nav-link');
+
+//     navLinks.forEach(link => {
+//         link.addEventListener('click', () => {
+//             navList.classList.remove('active');
+//             burger.classList.remove('toggle');
+//             document.body.classList.remove('lock-scroll');
+//         });
+//     });
+// });
--- js/modal.js
+++ js/modal.js
@@ -1,77 +1,77 @@
-// const openModalBtns = document.querySelectorAll(".openModalBtn");
-
-// openModalBtns.forEach(btn => {
-//   btn.addEventListener("click", function() {
-//     const modalId = this.dataset.modal; // Отримуємо id модального вікна з атрибута data-modal
-//     const modal = document.getElementById(modalId); // Знаходимо модальне вікно за id
-//     if (modal) {
-//       modal.style.display = "block"; // Показуємо модальне вікно, змінюючи стиль на "block"
-//       document.body.classList.add('lock-scroll');
-
-
-//       const closeModalBtn = modal.querySelector(".closeModalBtn");
-//       if (closeModalBtn) {
-//         closeModalBtn.addEventListener("click", function() {
-//           modal.style.display = "none"; // Ховаємо модальне вікно, змінюючи стиль на "none"
-//           document.body.classList.remove('lock-scroll');
-//           closeModalBtn.removeEventListener("click", closeModalBtn);
-//         });
-//       }
-//       window.addEventListener("click", function(event) {
-//         const modals = document.querySelectorAll(".modal");
-//         modals.forEach(modal => {
-//           if (event.target === modal) {
-//             modal.style.display = "none"; // Видаліть клас для закриття модального вікна
-//             document.body.classList.remove('lock-scroll');
-//             window.removeEventListener("click", window);
-//           }
-//         });
-//       });
-//     }
-//   });
-// });
-
 const openModalBtns = document.querySelectorAll(".openModalBtn");
 
 openModalBtns.forEach(btn => {
   btn.addEventListener("click", function() {
     const modalId = this.dataset.modal; // Отримуємо id модального вікна з атрибута data-modal
-    const modal = document.getElementById(modalId);
-    
-    // Якщо модальне вікно ще не завантажене, завантажуємо його контент
-    if (!modal) {
-      fetch(`/path/to/modal-content/${modalId}.html`)
-        .then(response => response.text())
-        .then(data => {
-          const modalContainer = document.createElement('div');
-          modalContainer.innerHTML = data;
-          document.body.appendChild(modalContainer);
+    const modal = document.getElementById(modalId); // Знаходимо модальне вікно за id
+    if (modal) {
+      modal.style.display = "block"; // Показуємо модальне вікно, змінюючи стиль на "block"
+      document.body.classList.add('lock-scroll');
 
-          const loadedModal = document.getElementById(modalId);
-          showAndBindModal(loadedModal);
+
+      const closeModalBtn = modal.querySelector(".closeModalBtn");
+      if (closeModalBtn) {
+        closeModalBtn.addEventListener("click", function() {
+          modal.style.display = "none"; // Ховаємо модальне вікно, змінюючи стиль на "none"
+          document.body.classList.remove('lock-scroll');
+          closeModalBtn.removeEventListener("click", closeModalBtn);
         });
-    } else {
-      showAndBindModal(modal);
+      }
+      window.addEventListener("click", function(event) {
+        const modals = document.querySelectorAll(".modal");
+        modals.forEach(modal => {
+          if (event.target === modal) {
+            modal.style.display = "none"; // Видаліть клас для закриття модального вікна
+            document.body.classList.remove('lock-scroll');
+            window.removeEventListener("click", window);
+          }
+        });
+      });
     }
   });
 });
 
-function showAndBindModal(modal) {
-  modal.style.display = "block"; // Показуємо модальне вікно, змінюючи стиль на "block"
-  document.body.classList.add('lock-scroll');
+// const openModalBtns = document.querySelectorAll(".openModalBtn");
 
-  const closeModalBtn = modal.querySelector(".closeModalBtn");
-  if (closeModalBtn) {
-    closeModalBtn.addEventListener("click", function() {
-      modal.style.display = "none"; // Ховаємо модальне вікно, змінюючи стиль на "none"
-      document.body.classList.remove('lock-scroll');
-    });
-  }
+// openModalBtns.forEach(btn => {
+//   btn.addEventListener("click", function() {
+//     const modalId = this.dataset.modal; // Отримуємо id модального вікна з атрибута data-modal
+//     const modal = document.getElementById(modalId);
+    
+//     // Якщо модальне вікно ще не завантажене, завантажуємо його контент
+//     if (!modal) {
+//       fetch(`/path/to/modal-content/${modalId}.html`)
+//         .then(response => response.text())
+//         .then(data => {
+//           const modalContainer = document.createElement('div');
+//           modalContainer.innerHTML = data;
+//           document.body.appendChild(modalContainer);
 
-  window.addEventListener("click", function(event) {
-    if (event.target === modal) {
-      modal.style.display = "none"; // Ховаємо модальне вікно, змінюючи стиль на "none"
-      document.body.classList.remove('lock-scroll');
-    }
-  });
-}
+//           const loadedModal = document.getElementById(modalId);
+//           showAndBindModal(loadedModal);
+//         });
+//     } else {
+//       showAndBindModal(modal);
+//     }
+//   });
+// });
+
+// function showAndBindModal(modal) {
+//   modal.style.display = "block"; // Показуємо модальне вікно, змінюючи стиль на "block"
+//   document.body.classList.add('lock-scroll');
+
+//   const closeModalBtn = modal.querySelector(".closeModalBtn");
+//   if (closeModalBtn) {
+//     closeModalBtn.addEventListener("click", function() {
+//       modal.style.display = "none"; // Ховаємо модальне вікно, змінюючи стиль на "none"
+//       document.body.classList.remove('lock-scroll');
+//     });
+//   }
+
+//   window.addEventListener("click", function(event) {
+//     if (event.target === modal) {
+//       modal.style.display = "none"; // Ховаємо модальне вікно, змінюючи стиль на "none"
+//       document.body.classList.remove('lock-scroll');
+//     }
+//   });
+// }
