651de699ac28aa44c2b292a0a3be76260014c209
kristinaordenans <okristin92@gmail.com>
2024-08-13 09:15:47 +0300

    clear js code

--- js/swiper.js
+++ js/swiper.js
@@ -1,12 +1,4 @@
-// core version + navigation, pagination modules:
-// import Swiper from 'swiper';
 import Swiper from "https://cdn.jsdelivr.net/npm/swiper@8/swiper-bundle.esm.browser.min.js";
-// import { Navigation, Pagination } from 'swiper/modules';
-// import Swiper and modules styles
-// import 'swiper.css';
-// import 'swiper/css/navigation';
-// import 'swiper/css/pagination';
-
 
 document.addEventListener('DOMContentLoaded', function() {
     const swiper = new Swiper('.swiper', {
